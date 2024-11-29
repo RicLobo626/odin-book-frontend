6d467d542666ed0d61023ca4952adf0c54841420
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-11-29 23:28:04 +0000

    Add optional icon props to Input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,18 +1,43 @@
+import { LucideIcon } from "lucide-react";
+import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
-import { ComponentProps, forwardRef } from "react";
 
-const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: LucideIcon;
+  endIcon?: LucideIcon;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { className, type, startIcon: StartIcon, endIcon: EndIcon, ...props },
+    ref,
+  ) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "focus-visible:ring-primary flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-base ring-offset-white transition-all duration-200 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-neutral-950 placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:file:text-neutral-50 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 md:text-sm",
-          className,
+      <div className="relative w-full">
+        {StartIcon && (
+          <div className="absolute left-1.5 top-1/2 -translate-y-1/2 transform">
+            <StartIcon size={18} className="text-muted-foreground ml-1" />
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+
+        <input
+          type={type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-base ring-offset-white transition-all duration-200 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-neutral-950 placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:file:text-neutral-50 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 md:text-sm",
+            className,
+            { "pl-9": StartIcon, "pr-9": EndIcon },
+          )}
+          ref={ref}
+          {...props}
+        />
+
+        {EndIcon && (
+          <div className="absolute right-3 top-1/2 mr-1 -translate-y-1/2 transform">
+            <EndIcon className="text-muted-foreground" size={18} />
+          </div>
+        )}
+      </div>
     );
   },
 );
