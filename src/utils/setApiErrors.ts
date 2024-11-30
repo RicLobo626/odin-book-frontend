8755c6cadd351c781dc4ddfdfd54379a5211e28a
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-11-30 16:55:32 +0000

    Update type parameters in setApiErrors

--- src/utils/setApiErrors.ts
+++ src/utils/setApiErrors.ts
@@ -1,13 +1,13 @@
 import { ApiError } from "@/errors";
-import { Path, UseFormSetError } from "react-hook-form";
+import { FieldPath, FieldValues, UseFormSetError } from "react-hook-form";
 
-export const setApiErrors = <T extends object>(
+export const setApiErrors = <T extends FieldValues = FieldValues>(
   error: ApiError,
   setError: UseFormSetError<T>,
 ) => {
   for (const key in error.details) {
     const message = error.details[key];
 
-    setError(key as Path<T>, { message });
+    setError(key as FieldPath<T>, { message });
   }
 };
